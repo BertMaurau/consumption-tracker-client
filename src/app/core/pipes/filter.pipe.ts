efb9709def8ffd58a6af84821d10b7504a11c03e
Bert Maurau <hello@bertmaurau.be>
2021-06-05 16:28:08 +0200

    check for string

--- src/app/core/pipes/filter.pipe.ts
+++ src/app/core/pipes/filter.pipe.ts
@@ -8,7 +8,13 @@ export class FilterPipe implements PipeTransform {
     transform(items: any[], field: string, value: string): any[] {
         if (!items) return [];
         if (!value || value.length == 0) return items;
-        return items.filter(it =>
-            it[field].toLowerCase().indexOf(value.toLowerCase()) != -1);
+
+        return items.filter((item: any) => {
+            if (typeof item[field] === 'string') {
+                return item[field].toLowerCase().indexOf(value.toLowerCase()) != -1;
+            } else {
+                return item[field] === value;
+            }
+        })
     }
 }
